1bb61610832cee8deb33eadc72b2c05cd9b661d4
hellooox3 <hellooox3@outlook.com>
2024-09-05 17:21:19 +0800

    basic functions, hook not included

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import CardList from "./CardList";
+import SearchBar from "./SearchBar";
+import Scroll from './Scroll';
+import ErrorBoundry from "./ErrorBoundry";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+       
+    }
+    render(){
+        const {robots, searchfield} = this.state;
+        const filterdRobot = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        if(robots.length === 0){
+            return <h1>Loading...</h1>
+        }
+        // else{
+            return(
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBar searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundry>
+                        <CardList robots={filterdRobot}/>
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            )
+        // }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => 
+                {
+                    // console.log('res: ',response);
+                    return response.json();
+                }
+            )
+            .then(users => this.setState({robots: users}));    
+    }
+
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         // .then(users => this.setState({robots: users}));
+    //         .then(info => console.log(info))
+    // }
 }
+
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id:1,
+        name:'Leane Graham',
+        username:'Bret',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:2,
+        name:'Leasdfa Graham',
+        username:'Bret',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:3,
+        name:'Lgfh45ne Graham',
+        username:'Bret',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:4,
+        name:'Lefsfane Graham',
+        username:'Bret',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:5,
+        name:'Lea536ne Graham',
+        username:'Bret',
+        email:'Sincere@april.biz'
+    }
+]
